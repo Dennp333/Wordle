93d8f40f758ef979471e6dc178259d49abc699b5
Dennp333 <77082803+Dennp333@users.noreply.github.com>
2022-03-16 22:07:07 -0400

    code cleanup

--- wordleBot.py
+++ wordleBot.py
@@ -90,7 +90,6 @@ class Solver:
             return self.currentWord
         if self.isInputValid(result):
             self.round += 1
-            #self.solutions = list(filter(lambda x: getResult(self.currentWord, x) == result, self.solutions))
             if self.round == 2:
                 self.filterSolutions(result)
                 choices = {}
@@ -113,68 +112,3 @@ class Solver:
             del self.entropies[self.currentWord]
             self.selectFromEntropyDict(self.entropies)
         return self.currentWord
-
-allWords = []
-with open("words.txt", "r") as file:
-    allWords = file.read().strip().replace("\"", "").split(",")
-solutions = []
-with open("words.txt", "r") as file:
-    solutions = file.read().strip().replace("\"", "").split(",")
-solver = Solver(allWords, solutions)
-result = "00000"
-while solver.round < 6:
-    word = solver.playRound(result)
-    print(f"Next word: {word}")
-    result = input("Enter the result: ")
-    if result == "22222":
-        print(f"Possible words remaining: {','.join(solver.solutions)}")
-        break
-
-# allWords = []
-# with open("words.txt", "r") as file:
-#     allWords = file.read().strip().replace("\"", "").split(",")
-# solutions = []
-# with open ("words.txt", "r") as file:
-#     solutions = file.read().strip().replace("\"", "").split(",")
-# checked = 0
-# start = False
-# beginningWord = "patty"
-# startTime = time.time()
-# for word in solutions:
-#     if not start:
-#         if word == beginningWord:
-#             start = True
-#         else:
-#             checked += 1
-#             continue
-#     solver = Solver(allWords, solutions)
-#     result = ""
-#     choice = ""
-#     while solver.round < 6:
-#         choice = solver.playRound(result)
-#         if choice == word:
-#             break
-#         result = getResult(choice, word)
-#     if choice != word:
-#         print(f"Failed: {word}")
-#         with open("results.txt", "a") as file:
-#             file.write(f"{word} failed\n")
-#     else:
-#         with open("results.txt", "a") as file:
-#             file.write(f"{word} passed\n")
-#     checked += 1
-#     if (checked % 100 == 0):
-#         print(f'Checked {checked} words')
-#         currentTime = time.time()
-#         remaining = (12947 - checked) / 100 * (currentTime - startTime)
-#         startTime = currentTime
-#         print(f"Remaining time: {datetime.timedelta(seconds=remaining)}")
-# failed = 0
-# with open("results.txt", "r") as file:
-#     saved = file.readlines()
-#     for entry in saved:
-#         line = entry.strip().split()
-#         if line[1] == "failed":
-#             failed += 1
-
-# print(f'Passed {12947 - failed} / 12947 words')
